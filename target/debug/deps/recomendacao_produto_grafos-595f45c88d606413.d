3022834adde06bede4afc3bfe72e10df6046ad0b
DevRuel <henrique.ruel123@gmail.com>
2025-05-21 23:57:32 +0000

    Commit inicial de produtos com grafos

---
+++ target/debug/deps/recomendacao_produto_grafos-595f45c88d606413.d
@@ -0,0 +1,5 @@
+/workspaces/recomendacao-produto-grafos/recomendacao-produto-grafos/target/debug/deps/recomendacao_produto_grafos-595f45c88d606413: src/main.rs
+
+/workspaces/recomendacao-produto-grafos/recomendacao-produto-grafos/target/debug/deps/recomendacao_produto_grafos-595f45c88d606413.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/recomendacao-produto-grafos.d
@@ -0,0 +1,1 @@
+/workspaces/recomendacao-produto-grafos/recomendacao-produto-grafos/target/debug/recomendacao-produto-grafos: /workspaces/recomendacao-produto-grafos/recomendacao-produto-grafos/src/main.rs
